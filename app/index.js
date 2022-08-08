c513eb133414db8ef99e32c4177e1e5b5bc04712
Lara Namiq <larasnamiq@gmail.com>
2022-08-08 14:27:53 +0100

    Complete tasks

--- app/index.js
+++ app/index.js
@@ -12,16 +12,18 @@ console.log(customer.firstName);
 
 // Task 2
 
-customer.street = "Abc Street";
-customer.city = "London";
-customer.state = "England";
-customer.zipcode = "AB12 3CD";
+customer.address = {
+  street: "Abc Street",
+  city: "London",
+  state: "England",
+  zipcode: "AB12 3CD",
+};
 
-console.log(customer.zipcode);
+console.log(customer.address);
 
 // Task 3
 
-console.log(`Welcome, ${customer.firstName} + ${customer.lastName} + '!'`);
+console.log(`Welcome, ${customer.firstName} + ${customer.lastName} !`);
 
 // Task 4
 
@@ -35,22 +37,14 @@ const product = {
 console.log(product.price);
 
 // Task 5
-=
 
-const total = (product.quantity + 7) * product.price;
-
-console.log(`Total: ${total}`);
+const total = product.price * 7;
+console.log(`Total: $${product.price * 7}`);
 
 // Task 6
 
-if (product.total > 100) {
-  console.log("true")
-}
+console.log(total >= 100);
 
-/**
- * TODO: Assign your own values to meet the conditions below:
+// Task 7
 
- * 6. Log a new message that prints either 'true' or 'false' to the screen depending on whether or not the customer spent over $100. Use conditional operators to accomplish this. Don't worry about the '$' symbol. Just use the number 100.
- *
- * 7. Update `product` to reflect the new quantity. In other words, the new quantity value should be 7 less than whatever you initialized it with.
- */
+product.quantity -= 7;
